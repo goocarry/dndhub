ea1ed3d149aa389bb7b629f1f2a1bbfdf3d9c249
goocarry <goocarry@gmail.com>
2023-03-14 16:08:40 +0900

    add auth handler

--- services/auth/data/models.go
+++ services/auth/data/models.go
@@ -1,8 +1,12 @@
 package data
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 const dbTimeout = time.Second * 3
@@ -25,10 +29,55 @@ type Models struct {
 
 // User model.
 type User struct {
-	ID        string    `json:"id"`
-	Email     string    `json:"email"`
-	FirstName string    `json:"first_name,omitempty"`
-	LastName  string    `json:"last_name,omitempty"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID         string    `json:"id"`
+	Email      string    `json:"email"`
+	FirstName  string    `json:"first_name,omitempty"`
+	LastName   string    `json:"last_name,omitempty"`
+	Password   string    `json:"password"`
+	UserActive bool      `json:"user_active"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+// GetByEmail ...
+func (u *User) GetByEmail(email string) (*User, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	var user User
+
+	r := db.QueryRowContext(ctx, "SELECT id, email, first_name, last_name, password, user_active, created_at, updated_at FROM public.users WHERE email=$1", email)
+
+	err := r.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.UserActive,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// PasswordMatches uses Go's bcrypt package to compare a user supplied password
+// with the hash we have stored for a given user in the database. If the paswword
+// and hash match, we return true; otherwise, we return false.
+func (u *User) PasswordMatches(plainText string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(plainText), []byte(u.Password))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
 }
