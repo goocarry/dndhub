8082a395d29cfeacc03cc50eba709d228148a459
Stanislav Kuzakov <goocarry@gmail.com>
2023-05-16 00:04:13 +0900

    vuetify gym initial frontend

---
+++ frontend/display/src/layouts/default/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <p class="font-weight-black"><v-icon class="mx-2" icon="mdi-dumbbell" />MarathonApp</p>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup>
+//
+</script>
