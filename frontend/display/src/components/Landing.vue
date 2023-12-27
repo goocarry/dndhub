3ec5aecc01a6b7fdb698219a41361f6b591d3e3e
Stanislav Kuzakov <s.kuzakov@surfer.media>
2023-12-27 23:27:03 +0900

    some

---
+++ frontend/display/src/components/Landing.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="fill-height" style="max-height: 100vh">
+    <v-img class="bg-white fill-height" cover src="@/assets/gym.jpg">
+      <v-col class="flex-column justify-space-between" style="height: 20vh">
+        <div>
+          <p class="ma-4 text-h5 font-weight-black text-white">
+            <v-icon class="mx-2" icon="mdi-dumbbell" />
+            MarathonApp
+          </p>
+        </div>
+        <div class="fill-height"></div>
+        <div class="ma-6">
+          <p class="ma-4 text-h4 font-weight-black text-white">Персональные тренировки</p>
+          <p class="ma-4 text-h4 font-weight-black text-white">от Анатолия Бицепса</p>
+        </div>
+        <div class="ma-6 fill-width">
+          <Timer />
+        </div>
+        <div class="ma-6 fill-width">
+          <v-btn class="ma-4 bg-white" prepend-icon="mdi-dumbbell" variant="tonal"> Начать! </v-btn>
+        </div>
+      </v-col>
+    </v-img>
+  </div>
+</template>
+
+<script>
+import Timer from './Timer.vue'
+export default {
+  components:{ Timer },
+}
+</script>
---
+++ frontend/display/src/components/Timer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="mx-4">
+        <p class="text-h4 font-weight-black text-white">До старта марафона осталось:</p>
+    </div>
+    <div class="mx-4 text-white">
+        {{ displayDays }}
+        {{ displayHours }}
+        {{ displayMinutes }}
+        {{ displaySeconds }}
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        displayDays: 0,
+        displayHours: 0,
+        displayMinutes: 0,
+        displaySeconds: 0,
+    }),
+}
+</script>
